9435a5527a255363a2c159394b899c9c96c4f253
shashi Bhushan <bhushans727@gmail.com>
2022-09-05 16:36:22 +0530

    added the missing part

--- src/components/FeedForm.jsx
+++ src/components/FeedForm.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import DetailsList from "./DetailsList";
+import './Styling.css'
 
 class FeedForm extends React.Component{
     constructor(props)
@@ -7,29 +9,13 @@ class FeedForm extends React.Component{
         this.state = {
             name : '',
             dept : '',
-            rate : ''
+            rate : '',
+            users:[]
         }
-        const data = this.state
-    }
-    
-    
-           
-    
-
-    // const data = this.state;
-    handleSubmit = (event) => {
-        console.log(this.state.name);
-        console.log(this.state.dept);
-        console.log(this.state.rate);
-
-        console.log(this.data.name);
-        console.log(this.data.dept);
-        console.log(this.data.rate);
-        
-        event.preventDefault();
         
     }
-
+    
+    
     handleNameChange = (event) => {
         this.setState({name:event.target.value})
     }
@@ -39,32 +25,43 @@ class FeedForm extends React.Component{
     handleRateChange = (event) => {
         this.setState({rate:event.target.value})
     }
+           
+    
+
+    // const data = this.state;
+    handleSubmit = (event) => {
+    const updateUsers = this.state.users;
+    updateUsers.push({name:this.state.name , dept:this.state.dept , rate:this.state.rate})
+    this.setState({users:updateUsers , name:'', dept:'' , rate:''})
+        
+    event.preventDefault();
+    }
+
 
     render(){
-
         return(
-            <>
-            <h1 className="feedHeading">EMPLOYEE FEEDBACK FORM</h1>
-            <form>
-                <div>
-                    <label htmlFor="name">Name : </label>
-                    <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+            <div className="container">
+                <h1 className="feedHeading">EMPLOYEE FEEDBACK FORM</h1>
+                <form>
+                    <div className="formElem">
+                        <label htmlFor="name">Name : </label>
+                        <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+                    </div>
+                    <div className="formElem">
+                        <label htmlFor="dept">Department : </label>
+                        <input type="text" value={this.state.dept} onChange={this.handleDeptChange}/>
+                    </div>
+                    <div className="formElem">
+                        <label htmlFor="rate">Rating : </label>
+                        <input type="number" value={this.state.rate} onChange={this.handleRateChange}/>
+                    </div>
+                    
+                    <button type="submit" onClick={this.handleSubmit}>Submit</button>
+                </form>
+                <div className="detailsContainer">
+                    <DetailsList name={this.state.name} dept={this.state.dept} rate={this.state.rate} users={this.state.users}/>
                 </div>
-                <div>
-                    <label htmlFor="dept">Department : </label>
-                    <input type="text" value={this.state.dept} onChange={this.handleDeptChange}/>
-                </div>
-                <div>
-                    <label htmlFor="rate">Rating : </label>
-                    <input type="text" value={this.state.rate} onChange={this.handleRateChange}/>
-                </div>
-                
-                <button type="submit" onClick={this.handleSubmit}>submit</button>
-            </form>
-            <div>
-                <h1>{this.state.name}</h1>
             </div>
-            </>
         )
     }
 
